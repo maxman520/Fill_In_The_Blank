04b8df8ec03b54bfd513a4d0e8094aef6e544e88
maxman520 <maxman520@naver.com>
2023-12-07 16:34:27 +0900

    난이도 선택 activity 추가

---
+++ app/src/main/java/com/example/fillintheblank/ChooseDifficulty.kt
@@ -0,0 +1,11 @@
+package com.example.fillintheblank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseDifficulty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_difficulty)
+    }
+}
