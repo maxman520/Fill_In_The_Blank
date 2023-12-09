c502c333802c97ce46d9cb6897c432694463d2ad
maxman520 <maxman520@naver.com>
2023-12-09 17:26:21 +0900

    build gradle 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,10 @@ android {
 }
 
 dependencies {
-
+    val activity_version = "1.6.1"
+    val lifecycle_version = "2.5.1"
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
